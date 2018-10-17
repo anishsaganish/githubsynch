873c8982aca6d3cecdc61121a756fe5b17416289
Anish Das <anish.das@capgemini.com>
2018-10-16 20:18:38 -0500

    Commit

---
+++ Database2/Database2/dbo/Tables/dept.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[dept] (
+    [deptno] INT          NOT NULL,
+    [dname]  VARCHAR (14) NULL,
+    [loc]    VARCHAR (13) NULL,
+    PRIMARY KEY CLUSTERED ([deptno] ASC)
+);
+
---
+++ Database2/Database2/dbo/Tables/emp.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[emp] (
+    [empno]    INT            NOT NULL,
+    [ename]    VARCHAR (10)   NULL,
+    [job]      VARCHAR (9)    NULL,
+    [mgr]      INT            NULL,
+    [hiredate] DATETIME       NULL,
+    [sal]      NUMERIC (7, 2) NULL,
+    [comm]     NUMERIC (7, 2) NULL,
+    [dept]     INT            NULL,
+    PRIMARY KEY CLUSTERED ([empno] ASC),
+    FOREIGN KEY ([dept]) REFERENCES [dbo].[dept] ([deptno])
+);
+
